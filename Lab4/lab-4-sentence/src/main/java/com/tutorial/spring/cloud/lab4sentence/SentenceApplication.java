65d58510d0c4b9acd1e852bc79bb3107ac345ea2
Jorge Lara <larajorge11@gmail.com>
2020-04-29 12:08:44 -0500

    Jorge Lara - create and update client eureka - sentence

--- Lab4/lab-4-sentence/src/main/java/com/tutorial/spring/cloud/lab4sentence/SentenceApplication.java
+++ Lab4/lab-4-sentence/src/main/java/com/tutorial/spring/cloud/lab4sentence/SentenceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class Lab4SentenceApplication {
+public class SentenceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Lab4SentenceApplication.class, args);
+		SpringApplication.run(SentenceApplication.class, args);
 	}
 
 }
