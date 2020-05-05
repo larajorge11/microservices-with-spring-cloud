551fcf8ef61ecf53d75e5c804f93c3b197dbf1c6
Jorge Lara <larajorge11@gmail.com>
2020-05-05 11:28:18 -0500

    Jorge Lara - Add rest controller annotation organization

--- Project_Davor_Eureka/OrganizationService/src/main/java/com/project/davor/eureka/OrganizationService/controller/OrganizationController.java
+++ Project_Davor_Eureka/OrganizationService/src/main/java/com/project/davor/eureka/OrganizationService/controller/OrganizationController.java
@@ -7,13 +7,11 @@ import com.project.davor.eureka.OrganizationService.repository.OrganizationRepos
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
 public class OrganizationController {
 
     private static Logger LOGGER = LoggerFactory.getLogger(OrganizationController.class);
